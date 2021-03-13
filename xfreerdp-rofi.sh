9165ff6a140400f585483d2d037e6878a257e8fb
CompileNix <compilenix@gmail.com>
2021-03-13 19:19:07 +0100

    add wl-xwayland-get-active-output.sh to rofi command

--- xfreerdp-rofi.sh
+++ xfreerdp-rofi.sh
@@ -3,6 +3,6 @@ dir=$(dirname $(ls -l "${BASH_SOURCE[0]}" | awk '{print $NF}'))
 
 pushd $dir >/dev/null
 ./app.py list | \
-    rofi -i -scroll-method 1 -dmenu | \
+    rofi -i -scroll-method 1 -dmenu -m $(wl-xwayland-get-active-output.sh) | \
     xargs -0 ./app.py run
 
